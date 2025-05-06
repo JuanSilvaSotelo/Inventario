8ad27cd0c126694860f85928fd201038b8b2542d
JULIORODR <149906090+JULIORODR@users.noreply.github.com>
2025-05-05 19:53:33 -0500

    corrijo cambio en la carpeta agregar

--- agregar.py
+++ agregar.py
@@ -1,5 +1,5 @@
 def agregar_producto(nombre):
-    """agregar un producto a la lista de productos"""
+    """Agregar un producto a la lista de productos"""
     producto = {"nombre": nombre}
     productos.append(producto)
     print(f"producto '{nombre}' agregado correctamente.")
