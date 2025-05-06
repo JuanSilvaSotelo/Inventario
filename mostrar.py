95160289c0e4f3822ae607f1de3e7ac36ccb4ff4
JULIORODR <149906090+JULIORODR@users.noreply.github.com>
2025-05-05 20:11:27 -0500

    se elimina la carpeta mostrar

--- mostrar.py
+++
@@ -1,7 +0,0 @@
-def mostrar_inventario(nombre):
-    """mostrar el inventario de un producto"""
-    for producto in productos:
-        if producto["nombre"] == nombre:
-            print(f"inventario de '{nombre}': {producto['inventario']}")
-            return
-    print(f"producto '{nombre}' no encontrado en la lista.")
